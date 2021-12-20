323db25eb02536aff0ebdc59b7bec79a0e419c0a
Ping Wu <wpwupingwp@outlook.com>
2021-12-20 19:39:53 +0800

    set export encoding

--- export.sql
+++ export.sql
@@ -1,7 +1,31 @@
-COPY (SELECT tree_id FROM trees) TO 'e:\\linux\plant_tree_db\\tree_id.csv' WITH (format csv, header);
+-- psql -U root -d treebasedmp -f sql.file
+-- COPY (SELECT tree_id FROM trees) TO 'e:\\linux\plant_tree_db\\tree_id.csv' WITH (format csv, header);
+-- tree id list
+COPY (SELECT DISTINCT tree_id FROM trees ORDER BY tree_id)
+TO 'e:\\linux\plant_tree_db\\tree_id.csv'
+WITH (format csv, header, encoding 'UTF8');
+-- trees
+COPY (SELECT DISTINCT * FROM trees ORDER BY tree_id)
+TO 'e:\\linux\plant_tree_db\\trees.csv'
+WITH (format csv, header, encoding 'UTF8');
+-- analysis
+COPY (SELECT DISTINCT analysis.analysisstep_id AS analysis_id, study_id,
+software, algorithm, tree_id
+FROM analysis LEFT JOIN analysis_tree
+ON analysis.analysisstep_id=analysis_tree.analysisstep_id
+ORDER BY analysis.analysisstep_id) TO 'e:\\linux\plant_tree_db\\analysis.csv'
+WITH (format csv, header, encoding 'UTF8');
+-- matrix
+COPY (SELECT DISTINCT * FROM matrix ORDER BY matrix_id)
+TO 'e:\\linux\plant_tree_db\\matrix.csv'
+WITH (format csv, header, encoding 'UTF8');
+-- study
+COPY (SELECT DISTINCT * FROM study ORDER BY study_id)
+TO 'e:\\linux\plant_tree_db\\matrix.csv'
+WITH (format csv, header, encoding 'UTF8');
 
-CREATE VIEW temp AS
-SELECT DISTINCT analysis.analysisstep_id AS analysis_id, study_id, software, algorithm, tree_id
-FROM analysis LEFT JOIN analysis_tree ON
-analysis.analysisstep_id=analysis_tree.analysisstep_id
-ORDER BY analysis.analysisstep_id;
+--select distinct *  from study left join analysis on
+--study.study_id=analysis.study_id  left join analysis_tree
+--on analysis.analysisstep_id=analysis_tree.analysisstep_id left join matrix on
+--analysis.analysisstep_id=matrix.analysisstep_id
+--order by analysis.analysisstep_id ;
