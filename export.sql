e7549b1f0cef836ff673b9f9fad9d58691798923
Ping Wu <wpwupingwp@outlook.com>
2021-12-20 19:59:20 +0800

    export ok

--- export.sql
+++ export.sql
@@ -3,26 +3,26 @@
 -- tree id list
 COPY (SELECT DISTINCT tree_id FROM trees ORDER BY tree_id)
 TO 'e:\\linux\plant_tree_db\\tree_id.csv'
-WITH (format csv, header, encoding 'UTF8');
+WITH (format csv, header, encoding 'UTF8', delimiter ';');
 -- trees
 COPY (SELECT DISTINCT * FROM trees ORDER BY tree_id)
 TO 'e:\\linux\plant_tree_db\\trees.csv'
-WITH (format csv, header, encoding 'UTF8');
+WITH (format csv, header, encoding 'UTF8', delimiter ';');
 -- analysis
 COPY (SELECT DISTINCT analysis.analysisstep_id AS analysis_id, study_id,
 software, algorithm, tree_id
 FROM analysis LEFT JOIN analysis_tree
 ON analysis.analysisstep_id=analysis_tree.analysisstep_id
 ORDER BY analysis.analysisstep_id) TO 'e:\\linux\plant_tree_db\\analysis.csv'
-WITH (format csv, header, encoding 'UTF8');
+WITH (format csv, header, encoding 'UTF8', delimiter ';');
 -- matrix
 COPY (SELECT DISTINCT * FROM matrix ORDER BY matrix_id)
 TO 'e:\\linux\plant_tree_db\\matrix.csv'
-WITH (format csv, header, encoding 'UTF8');
+WITH (format csv, header, encoding 'UTF8', delimiter ';');
 -- study
 COPY (SELECT DISTINCT * FROM study ORDER BY study_id)
-TO 'e:\\linux\plant_tree_db\\matrix.csv'
-WITH (format csv, header, encoding 'UTF8');
+TO 'e:\\linux\plant_tree_db\\study.csv'
+WITH (format csv, header, encoding 'UTF8', delimiter ';');
 
 --select distinct *  from study left join analysis on
 --study.study_id=analysis.study_id  left join analysis_tree
