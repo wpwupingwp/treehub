a7f180b683531d42a2d8b0d0d50026d5b5c7785c
Ping Wu <wpwupingwp@outlook.com>
2022-12-22 16:59:21 +0800

    use another server?

--- web/views.py
+++ web/views.py
@@ -576,7 +576,7 @@ def submit_list(page=1):
 
 @app.route('/treedb/node/<int:tree_id>')
 def redirect_to_node(tree_id):
-    return f.redirect(f'http://localhost:4000/{tree_id}', code=302)
+    return f.redirect(f'/planttreenode/{tree_id}')
 
 
 @app.route('/')
