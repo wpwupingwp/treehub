12c0e498988e87eff1334222c43e06eaacfe4314
Ping Wu <wpwupingwp@outlook.com>
2024-02-16 21:28:57 +0800

    try to use requests to batch submit data

--- local_batch_submit.py
+++ local_batch_submit.py
@@ -1,35 +1,50 @@
 import json
+from pprint import pprint
+from pathlib import Path
 
 import requests
 
 # should clean bad submit before
 URL1 = 'http://localhost:2022/planttree/submit'
 URL2 = 'http://localhost:2022/planttree/submit/' # +n
-PAPER_JSON = 'new.json'
-TREE_JSON = 'tree.json'
-def submit(record: dict):
-    submit_form1 = {}
-    with requests.Session() as s:
-        r1 = s.post(URL1, data=submit_form1)
-        if r1.status_code == 200:
+MERGE_JSON = Path(r'R:\submit\merge.json')
+TREE_FOLDER = Path(r'R:\submit\trees')
+
+
+
+def submit(record: dict, trees:dict, session: requests.Session):
+    submit_form1 = record
+    pprint(submit_form1)
+    r1 = session.post(URL1, data=submit_form1)
+    if r1.status_code != 200:
+        pprint(r1.raw)
+        raise Exception(r1.status_code)
+    for n, tree in enumerate(trees[:-1]):
+        submit_form2 = tree
+        submit_form2['tree_file'] = submit_form2['tree_file'].read_bytes()
+        r2 = session.post(f'{URL2}{n}', data=submit_form2)
+        if r2.ok:
             pass
-        for n, tree in enumerate(record['trees'][:-1]):
-            submit_form2 = {}
-            r2 = s.post(f'{URL2}{n}', data=submit_form2)
-            if r2.ok:
-                pass
-        submit_form2 = record['trees'][-1]
-        submit_form2['submit'] = True
-    r3 = s.post(f'{URL2}{n}', data=submit_form2)
+    submit_form3 = trees[-1]
+    submit_form3['tree_file'] = submit_form3['tree_file'].read_bytes()
+    submit_form3['submit'] = True
+    r3 = session.post(f'{URL2}{n}', data=submit_form3)
     if r3.ok:
         print(r3.text)
 
 
 def main():
-    papers = json.load(open(PAPER_JSON, 'r'))
-    trees = json.load(open(TREE_JSON, 'r'))
-    for paper in papers:
-        pass
+    data = json.load(open(MERGE_JSON, 'r'))
+    session = requests.Session()
+    for key in data.keys():
+        record = data[key]
+        trees = list(record['tree_files'])
+        record.pop('tree_files')
+        for tree in trees:
+            tree['tree_file'] = TREE_FOLDER / Path(tree['tree_file'])
+            assert tree['tree_file'].exists(), tree['tree_file']
+        submit(record, trees, session)
+        break
     return
 
 
