1571f1855b15924e19de40f8340c583735cf99c9
a <a@a.com>
2025-11-25 17:26:51 +0800

    try to use systemd timer

--- run1.sh
+++ run1.sh
@@ -1,1 +1,1 @@
-cd /home/vms240301/auspice && node auspice.js view --datasetDir /dev/shm/treehub/tmp --verbose
+cd /home/vms240301/auspice && /home/vms240301/.local/bin/node auspice.js view --datasetDir /dev/shm/treehub/tmp --verbose
--- run_per_day.sh
+++ run_per_day.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 pkill node
 pkill hypercorn
 python3 /home/vms240301/plant_tree_db/remove_json.py
-nohup bash /home/vms240301/run1.sh &
-nohup bash /home/vms240301/run2.sh &
+nohup bash /home/vms240301/plant_tree_db/run1.sh &
+nohup bash /home/vms240301/plant_tree_db/run2.sh &
